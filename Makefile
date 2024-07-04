c1269ca1009f12a0a535aa02d4a2ffb3282c7abd
MikeKnight286 <bhonetayzarkyaw286@gmail.com>
2024-07-04 21:28:42 +0900

    Added libpng and libjpeg for image processing

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 # Makefile
 
 CC = gcc
-CFLAGS = -Iinclude
+CFLAGS = -Iinclude -I/mingw64/include
+LDFLAGS = -L/mingw64/lib -lpng -ljpeg -lz
 SRC = $(wildcard src/*.c)
 OBJ = $(SRC:.c=.o)
-TARGET = my_project
+TARGET = password_manager
 
 all: $(TARGET)
 
 $(TARGET): $(OBJ)
-	$(CC) -o $(TARGET) $(OBJ)
+	$(CC) -o $(TARGET) $(OBJ) $(LDFLAGS)
 
 %.o: %.c
 	$(CC) -c -o $@ $< $(CFLAGS)
--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
+#include <png.h>
+#include <jpeglib.h>
 #include <stdio.h>
 #include "utils.h"
 
 int main() {
-    printf("Hello Main File\n");
+    printf("Starting program...\n");
+    fflush(stdout);
+
+    printf("libpng version: %s\n", png_get_libpng_ver(NULL));
+    fflush(stdout);
+
+    printf("libjpeg version: %d\n", JPEG_LIB_VERSION);
+    fflush(stdout);
+
+    printf("Ending program...\n");
+    fflush(stdout);
+
     utils();
+
     return 0;
 }
-
