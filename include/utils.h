965b6713b4813c75c029cf5f33c3e8cfc40cf05d
MikeKnight286 <bhonetayzarkyaw286@gmail.com>
2024-08-02 16:40:58 +0900

    Defined utilities for generating fingerprints

--- include/utils.h
+++ include/utils.h
@@ -8,6 +8,9 @@ bool isValidEmail(const char *email);
 bool isStrongPassword(const char *password);
 void string_to_argon2hash(const char *input_string, char *hashed_string);
 bool verify_argon2hash(const char *input_string, const char *hashed_string);
+void generate_fingerprint_from_image(const char *input_image, const char *user_id, const char *user_email, char *image_fingerprint);
+void generate_key_from_image_fingerprint(const char *image_fingerprint, char *key, size_t key_len);
+void generate_key_from_password(const char *password, char *key, size_t key_len);
 bool isValidImage(const char *image_path, char *output_path);
 
 #endif
