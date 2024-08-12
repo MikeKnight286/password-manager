e85289bfedcb23e18d9d4013f2d4b98e6ba07df5
MikeKnight286 <bhonetayzarkyaw286@gmail.com>
2024-08-13 05:31:00 +0900

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -15,6 +15,8 @@ void generate_key_from_image_fingerprint(const char *image_fingerprint, char *ke
 void generate_key_from_password(const char *password, char *key, size_t key_len);
 void encrypt_data(const unsigned char *key, const unsigned char *plaintext, size_t plaintext_len, unsigned char **ciphertext, size_t *ciphertext_len);
 void decrypt_data(const unsigned char *key, const unsigned char *ciphertext, size_t ciphertext_len, unsigned char *plaintext, size_t *plaintext_len);
+bool embed_data_into_image(const char *input_image_path, const char *output_image_path, const unsigned char *ciphertext, size_t ciphertext_len);
+bool extract_data_from_image(const char *encrypted_image_path, unsigned char **ciphertext, size_t *ciphertext_len);
 void encrypt_credentials_into_image(const char *master_password_key, const char *master_image_key, const char *account_username, const char *account_domain, const char *account_pwd, const char *input_image, char *output_image);
 void decrypt_credentials_from_image(const unsigned char *master_password_key, const unsigned char *master_image_key, const char *input_image, char *account_username, char *account_domain, char *account_pwd);
 
