33709c7ba257155f2442ad1b93f2f7dbf7a840f4
MikeKnight286 <bhonetayzarkyaw286@gmail.com>
2024-08-20 19:21:38 +0900

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -22,11 +22,8 @@ bool encrypt_data(const unsigned char *key, const unsigned char *plaintext, size
 bool decrypt_data(const unsigned char *key, const unsigned char *ciphertext, size_t ciphertext_len, unsigned char *plaintext, size_t *plaintext_len);
 bool embed_data_into_image(const char *input_image_path, const char *output_image_path, const unsigned char *ciphertext, size_t ciphertext_len);
 bool extract_data_from_image(const char *encrypted_image_path, unsigned char **ciphertext, size_t *ciphertext_len);
-bool encrypt_credentials_into_image(const char *master_password_key, const char *master_image_key,
-                                    const char *account_username, const char *account_domain, const char *account_pwd,
-                                    const char *input_image, char *output_image);
-bool decrypt_credentials_from_image(const unsigned char *master_password_key, const unsigned char *master_image_key,
-                                    const char *input_image, char *account_username, char *account_domain, char *account_pwd,
-                                    size_t max_username_len, size_t max_domain_len, size_t max_password_len);
+bool encrypt_credentials_into_image(const char *master_password_key, const char *master_image_key, const char *account_username, const char *account_domain, const char *account_pwd, const char *input_image, char *output_image);
+bool decrypt_credentials_from_image(const unsigned char *master_password_key, const unsigned char *master_image_key, const char *input_image, char *account_username, char *account_domain, char *account_pwd, size_t max_username_len, size_t max_domain_len, size_t max_password_len);
+void generate_random_password(const char *username, const char *domain, char *password);
 
 #endif
